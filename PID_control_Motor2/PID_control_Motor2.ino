ca2021506b5785e4d35188e0f80f8e8492e74dfe
ryoryo666 <sakaryo0102@gmail.com>
2020-12-24 15:57:44 +0900

    school

--- PID_control_Motor2/PID_control_Motor2.ino
+++ PID_control_Motor2/PID_control_Motor2.ino
@@ -1,25 +1,21 @@
-#include <ros.h>
-#include <two_wheel/RL_RPM.h>
-#include <two_wheel/RightLeft_cmd_value.h>
-
 //Pin number
 #define R_encoderA 2
 #define R_encoderB 4
 #define L_encoderA 3
-#define L_encoderB 7
-#define r_motor_pwm 5
+#define L_encoderB 5
+#define r_motor_pwm 11
 #define l_motor_pwm 6
 #define IN1 12
 #define IN2 13
 
 
 //Parameter
-float Kp=6.0;
+float Kp=10.0;
 //float Ki=0.0;
-float Kd=4.0;
+float Kd=5.0;
 
-float r_Target = 0.0;
-float l_Target = 0.0;
+float r_Target = 10.0;
+float l_Target = 10.0;
 float r_data;
 float l_data;
 float r_last_data = 0.0;
@@ -29,30 +25,11 @@ float r_duty = 0.0;
 float l_duty = 0.0;
 float r_dt, r_preTime;
 float l_dt, l_preTime;
+float last_Time;
 float r_P, r_I, r_D, r_preP = 0;
 float l_P, l_I, l_D, l_preP = 0;
 volatile int r_encoderCnt=0;
 volatile int l_encoderCnt=0;
-
-ros::NodeHandle nh;
-//  Subscriber setting
-void messageCb(const two_wheel::RightLeft_cmd_value& new_target){
-  r_Target=new_target.r_ref;
-  l_Target=new_target.l_ref;
-  if(r_Target < 0 && l_Target < 0){
-    CCW();
-  }else if(r_Target > 0 && l_Target > 0){
-    CW();
-  }else if(r_Target == 0 && l_Target == 0){
-    STOP();
-  }
-}
-ros::Subscriber<two_wheel::RightLeft_cmd_value> sub("New_cmd", messageCb);
-
-
-//  Publisher setting
-two_wheel::RL_RPM msg;
-ros::Publisher chatter("rpm_data", &msg);
 
 void setup(){
   pinMode(R_encoderA, INPUT);
@@ -69,19 +46,18 @@ void setup(){
   attachInterrupt(0, Right_Motor, CHANGE);
   attachInterrupt(1, Left_Motor, CHANGE);
 
-  nh.initNode();
-  nh.advertise(chatter);
-  nh.subscribe(sub);
+  Serial.begin(115200);
 
   r_preTime=micros();
   l_preTime=r_preTime;
+  last_Time=micros();
 }
 
 void loop(){
   static int i=0;
   static float startTime=micros();
-  r_data=(float)r_encoderCnt*((2*3.1415)/1500)*200;  // [rad/s]
-  l_data=(float)l_encoderCnt*((2*3.1415)/1500)*200;  // [rad/s]
+  r_data=(float)r_encoderCnt/(6*250)*100*60;
+  l_data=(float)l_encoderCnt/(6*250)*100*60;
   r_data=0.01*r_data+(1-0.01)*r_last_data;
   l_data=0.01*l_data+(1-0.01)*l_last_data;  
 
@@ -90,43 +66,30 @@ void loop(){
 
   analogWrite(r_motor_pwm, abs(r_duty));
   analogWrite(l_motor_pwm, abs(l_duty));
-
-  i++;
-  if(i==10){
-    if(r_encoderCnt==0){
-      msg.r_data=0.0;
-    }else{
-      msg.r_data=r_data;
-    }
-    
-    if(l_encoderCnt==0){
-      msg.l_data=0.0;
-    }else{
-      msg.l_data=l_data;
-    }
-    chatter.publish(&msg);  // 20Hz Publish
-    i=0;
-    }
+  Serial.print(l_data);
+  Serial.print("\t");
+  Serial.println(r_data);
+  Serial.print("\n");
     
   r_encoderCnt=0;
   l_encoderCnt=0;
   r_last_data=r_data;
   l_last_data=l_data;
  
-  nh.spinOnce();
+  last_Time=micros();
   delay(5);
 }
 
 
 void R_PID(){
-  r_dt = (micros() - r_preTime) / 1000000;
+  r_dt = (micros() - r_preTime) / 1000000.0;
   r_preTime = micros();
   r_P  = r_Target - abs(r_data);
   //r_I += (r_P + r_preP)* r_dt;
   r_D  = (r_P - r_preP) / r_dt;
   r_preP = r_P;
 
-  r_duty += Kp * r_P /*+ Ki * r_I */+ Kd * r_D;
+  r_duty += Kp * r_P +/* Ki * I +*/ Kd * r_D;
   if(r_duty<0){
     r_duty=0;
   }else if(r_duty>250){
@@ -135,14 +98,14 @@ void R_PID(){
 }
 
 void L_PID(){
-  l_dt = (micros() - l_preTime) / 1000000;
+  l_dt = (micros() - l_preTime) / 1000000.0;
   l_preTime = micros();
   l_P  = l_Target - abs(l_data);
-  //l_I += (r_P + r_preP)* r_dt;
+  //r_I += (r_P + r_preP)* r_dt;
   l_D  = (l_P - l_preP) / l_dt;
   l_preP = l_P;
  
-  l_duty += Kp * l_P /*+ Ki * l_I */+ Kd * l_D;
+  l_duty += Kp * l_P +/* Ki * I +*/ Kd * l_D;
   if(l_duty<0){
     l_duty=0;
   }else if(l_duty>250){
@@ -170,20 +133,13 @@ void Left_Motor(){
 }
 
 void CW(){
-  STOP();
   digitalWrite(IN1, LOW);  
   digitalWrite(IN2, LOW);
 }
 
 void CCW(){
-  STOP();
   digitalWrite(IN1, HIGH);  
   digitalWrite(IN2, HIGH);
   r_Target = r_Target * -1;
   l_Target = l_Target * -1;
-}
-
-void STOP(){
-  digitalWrite(9,HIGH);
-  digitalWrite(8,HIGH);
 }
